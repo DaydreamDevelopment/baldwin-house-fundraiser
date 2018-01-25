556e3caf78ed3acff96a2a5ae97b948d3aa1ebdf
Brayden Girard <brayden@daydreamdev.com>
2018-01-24 22:56:33 -0500

    Fixed stripe

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,45 +1,5 @@
-// Create a Stripe client
-var stripe = Stripe('pk_live_gGXvGnVE3ZNAIKlB0wIz84gh');
-
-// Create an instance of Elements
-var elements = stripe.elements();
-
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-var style = {
-  base: {
-    color: '#32325d',
-    lineHeight: '18px',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
-    }
-  },
-  invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
-  }
-};
-
-// Create an instance of the card Element
-var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>
-card.mount('#card-element');
-
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
 function stripeTokenHandler(token) {
+  $.LoadingOverlay("show"); 
   var data = {
     firstName: $('#donate-first').val(),
     lastName: $('#donate-last').val(),
@@ -145,36 +105,7 @@ $(document).ready(function() {
     ticketsSelected = tempTicketsSelected;
     updateTotal();
   });
-  $("#donate-now-button").click(function() {
-    event.preventDefault();
-    $("#error-message-field").hide();
-    if(!$('#donate-first').val() && !$('#donate-last').val() &&
-       !$('#donate-address').val() && !$('#donate-postal').val() &&
-       !$('#donate-phone').val() && !$('#donate-email').val()) {
-      $("#error-message-field").text('You must complete all form fields.');
-      $("#error-message-field").show();
-      return;
-    }
-    if($('#donate-first').val().trim() == '' && $('#donate-last').val().trim() == '' &&
-       $('#donate-address').val().trim() == '' && $('#donate-postal').val().trim() == '' &&
-       $('#donate-phone').val().trim() == '' && $('#donate-email').val().trim() == '') {
-      $("#error-message-field").text('You must complete all form fields.');
-      $("#error-message-field").show();
-      return;
-    }
-    $.LoadingOverlay("show");
-    stripe.createToken(card).then(function(result) {
-      if (result.error) {
-        // Inform the user if there was an error
-        var errorElement = document.getElementById('card-errors');
-        errorElement.textContent = result.error.message;
-        $.LoadingOverlay("hide");
-      } else {
-        // Send the token to your server
-        stripeTokenHandler(result.token.id);
-      }
-    });
-  });
+  
   donationRef.on('value', function(snapshot) {
     var totalDonations = 0;
     snapshot.forEach(childSnapshot => {
